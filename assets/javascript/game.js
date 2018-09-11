2990239b87b38bc5b95180cce1703b457bb184b4
hughescoding <mhughes@myjao.org>
2018-09-11 17:04:01 -0400

    included correct JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,94 @@
+//tells me the JS is alive
 console.log('Application starting...')
 
-//- Create a list of words
+//defines the array of word choices
 
 var wordArray = ["mango", "apple", "orange", "grape", "watermelon", "strawberry", "plum",
-"pear", "kiwi", "papaya", "guava", "clementine", "peach", "grapefruit",]
+    "pear", "kiwi", "papaya", "guava", "clementine", "peach", "grapefruit",]
 
-//Press any key to start (how do you start it)
-// class = start-text (Make this go away when you start)
-
-var lettersGuessed = []
-var currentWord = []
-var wordToMatch
-var numGuess
+//Boolean to keep JS from running nutil user starts
+var gameStarted = false
+//need a starting point for Guesses
+var remainingGuesses = 0
+//need a starting point for wins counter
 var wins = 0
-
-// if letterGuessed.push(event.key.toLowerCase())
-// Next, we give Javascript a function to execute when onkeypress fires.
-
-document.onkeypress = function (event) {
-    var displayWords = document.getElementById("current-word");
-
-    //userText.textContent = event.key;
-     var randomWord = wordGenerator();
-     console.log("Random Word: " + randomWord);
-    var tempString = "<p>";
-   
-    // output the stuff
-    for (var i = 0; i < randomWord.length; i++) {
-        if (lettersGuessed.includes(randomWord[i])) {
-            tempString += randomWord[i] + " ";
-        } else {
-            tempString += "_ ";
-        }
-      
-    }
-    // end paragraph
-    tempString += "</p>";
-    console.log(tempString);
-    displayWords.innerHTML = tempString;
-    // Select one randomly (Random Word Generator)
-}
+//connecting the JS to the html so we can display output
+var currentWord = document.getElementById("current-word")
+var showLetters = document.getElementById("guessed-letters")
+var showremainingGuesses = document.getElementById("remaining-guesses")
+var showWins = document.getElementById("total-wins")
+//available choices
+var letterChoices = "abcdefghijklmnopqrstuvwxyz"
+//current random word
+var randomWord = []
+//picks word from wordArray
 var wordGenerator = function () {
-    var wordIndex = [Math.floor(Math.random() * wordArray.length)];
-    return wordArray[wordIndex];
+    word = wordArray[Math.floor(Math.random() * wordArray.length)].toLowerCase()
+    console.log(word);
+    return word
+}
+//game start function
+var start = function () {
+    gameStarted = true
+    blanks = []
+    //empty var for letters user presses
+    lettersGuessed = []
+    showLetters.innerHTML = lettersGuessed
+    randomWord = wordGenerator()
+    for (i = 0; i < randomWord.length; i++) {
+        blanks[i] = "_"
+    }
+    currentWord.innerHTML = blanks.join(" ")
+    remainingGuesses = 10
+    showremainingGuesses.innerHTML = remainingGuesses
+}
+var endGame = function () {
+    gameStarted = false
 }
 
-//- Display the word
-function updateDisplay() {
-    document.getElementById("total-wins").innerText = wins
-    document.getElementById("current-word").innerText = currentWord.join("")
-    document.getElementById("remaining-guesses").innerText = numGuess
-    document.getElementById("guessed-letters").innerText = guessedLetters.join(" ")
+document.onkeyup = function (event) {
+    var userEntry = event.key.toLowerCase()
+    if (gameStarted === false) {
+        start()
+    }
+    else {
+        if (gameStarted === true) {
+            //determine if key pressed is a match
+            if (letterChoices.includes(userEntry)) {
+                
+                //alert if the letter was used already
+                if (lettersGuessed.includes(userEntry)) {
+                    alert("You can only guess a letter once")
+                }
+                //add letter to list of guessed letters and subtract from remaining guesses
+                else {
+                    lettersGuessed.push(userEntry.toLowerCase())
+                    showLetters.innerHTML = lettersGuessed
+                    remainingGuesses--
+                    showremainingGuesses.innerHTML = remainingGuesses
+                }
+                //checks for letter matches & displays
+                for (i = 0; i < randomWord.length; i++) {
+                    if (randomWord[i] === userEntry) {
+                        blanks[i] = userEntry
+                        currentWord.innerHTML = blanks.join(" ")
+                        remainingGuesses++
+                    }
+                }
+            }
+            //game win or lose
+            if (remainingGuesses === 0) {
+                alert("You Lose! Try Again..")
+                endGame()
+            }
+            else if (!blanks.includes("_")) {
+                alert("You Win! Play Again..");
+                wins++;
+                endGame();
+                showWins.innerHTML = wins;
+                //didn't like that after the alert you had to hit a key for next word, added this
+                start();
+            }
+        }
+    }
 }
-// 1. Press any key to play again
---
+++ assets/javascript/game2.js
@@ -0,0 +1,57 @@
+console.log('Application starting...')
+
+//- Create a list of words
+
+var wordArray = ["mango", "apple", "orange", "grape", "watermelon", "strawberry", "plum",
+"pear", "kiwi", "papaya", "guava", "clementine", "peach", "grapefruit",]
+
+//Press any key to start (how do you start it)
+// class = start-text (Make this go away when you start)
+
+var lettersGuessed = []
+var currentWord = []
+var wordToMatch
+var numGuess
+var wins = 0
+
+// if letterGuessed.push(event.key.toLowerCase())
+// Next, we give Javascript a function to execute when onkeypress fires.
+
+document.onkeypress = function (event) {
+    var displayWords = document.getElementById("current-word");
+
+    //userText.textContent = event.key;
+     var randomWord = wordGenerator();
+     console.log("Random Word: " + randomWord);
+    var tempString = "<p>";
+   
+    // output the stuff
+    for (var i = 0; i < randomWord.length; i++) {
+        if (lettersGuessed.includes(randomWord[i])) {
+            tempString += randomWord[i] + " ";
+        } else {
+            tempString += "_ ";
+        }
+      
+    }
+    // end paragraph
+    tempString += "</p>";
+    console.log(tempString);
+    displayWords.innerHTML = tempString;
+    // Select one randomly (Random Word Generator)
+
+
+//- Display the word
+function updateDisplay() {
+    document.getElementById("total-wins").innerText = wins
+    document.getElementById("current-word").innerText = currentWord.join("")
+    document.getElementById("remaining-guesses").innerText = numGuess
+    document.getElementById("guessed-letters").innerText = guessedLetters.join(" ")
+}
+}
+var wordGenerator = function () {
+    var wordIndex = [Math.floor(Math.random() * wordArray.length)];
+    return wordArray[wordIndex];
+}
+
+// 1. Press any key to play again
